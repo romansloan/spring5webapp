df064ae605f811eaf89b9ad6666d9d81dc12dcd2
roman.a.sloan@gmail.com <roman.a.sloan@gmail.com>
2020-06-16 02:35:21 -0400

    Added Publisher.java and PublisherRepository.java.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -36,7 +40,11 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(rod);
         bookRepository.save(noEJB);
 
+        Publisher p1 = new Publisher("wiley", "123 Any street", "Anycity", "NY", "11111");
+        publisherRepository.save(p1);
+
         System.out.println("Started in Bootstrap ...");
         System.out.println("Number of books " + bookRepository.count());
+        System.out.println("Added publisher is " + publisherRepository.findAll());
     }
 }

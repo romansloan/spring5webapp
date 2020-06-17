9f12769407835f1527c12c712d1f5ed5bd5c53d5
roman.a.sloan@gmail.com <roman.a.sloan@gmail.com>
2020-06-17 02:15:46 -0400

    Updated Book.java to include the Publisher. Updated Publisher.java to include the set of books.

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -15,6 +15,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
                        inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -34,6 +37,14 @@ public class Book {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public String getTitle() {

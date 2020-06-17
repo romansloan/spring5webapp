d69476e466b7a9a9d7d6a2675ea072d5937e1c5a
roman.a.sloan@gmail.com <roman.a.sloan@gmail.com>
2020-06-17 03:31:39 -0400

    Displaying the list of books in HTML using Thymeleaf template.

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,9 +1,24 @@
 package guru.springframework.spring5webapp.controllers;
 
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BookController {
 
-    public String getBooks(){
+    private final BookRepository bookRepository;
 
-        return "books";
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books/list";
     }
 }
